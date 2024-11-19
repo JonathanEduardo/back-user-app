651a5b44809e084a9002a96b9d0418bc1515545c
Jonathan Quistiano <jhonquistiano@gmail.com>
2024-11-19 13:17:56 -0600

    Factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -21,8 +22,10 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('password'), // password
+            'remember_token' => \Illuminate\Support\Str::random(10), // Token aleatorio
+            'created_at' => now(), // Fecha de creación actual
+            'updated_at' => now(), // Fecha de actualización actual
         ];
     }
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,20 +1,21 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(UserSeeder::class);
+        // Crear 20 usuarios ficticios
+        User::factory()->count(50)->create();
     }
 }
